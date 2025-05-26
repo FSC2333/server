4c40f1a49dbb499955397bd833e1ad6d8cf94684
FSC2333 <140636129+FSC2333@users.noreply.github.com>
2025-05-26 21:43:57 +0800

    Add files via upload

---
+++ cache/byteview.go
@@ -0,0 +1,33 @@
+package lruCache
+
+// A ByteView holds an immutable view of bytes.
+type ByteView struct {
+	b []byte
+}
+
+func NewByteView(value []byte) *ByteView {
+	return &ByteView{
+		b: cloneBytes(value),
+	}
+}
+
+// Len returns the view's length
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+// ByteSlice returns a copy of the data as a byte slice.
+func (v ByteView) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
+// String returns the data as a string, making a copy if necessary.
+func (v ByteView) String() string {
+	return string(v.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
